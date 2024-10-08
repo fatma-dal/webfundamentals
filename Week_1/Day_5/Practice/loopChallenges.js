ca6048403a03dc4dc020dc86c3e4c035b0848f5c
fatma-dal <fatmadalhoumi123@gmail.com>
2024-10-08 14:53:57 +0200

    Loop challenges ||

---
+++ Week_1/Day_5/Practice/loopChallenges.js
@@ -0,0 +1,30 @@
+//  Print odds 1-20
+/*for (var i=0;i<=20;i++){
+    if (!(i%2) ){
+        print=console.log(i)
+    }
+}
+// Decreasing Multiples of 3
+for (var i=100;i>=0;i--){
+    if (i%3==0){
+        print=console.log(i)
+    }
+}
+//Sigma
+var sum
+for (var i=1;i<=100;i++){
+    sum+=i
+    console.log(sum)
+}
+//Print the sequence 
+for (var i=4;i>=-3.5;i-=1.5){
+}
+console.log(i)*/
+
+//factorial
+var product=1
+for(var i=1;i<13;i++){
+    product*=i
+    
+}
+console.log(product)
